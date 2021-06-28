3009f818c31935f3c1e490507b85b4db1cbe4f72
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-28 11:48:56 +0300

    axios .then test email

---
+++ functions/promise.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+
+const getUser = () => {
+    return axios
+        .get('https://jsonplaceholder.typicode.com/users/2')
+        .then(resp => resp.data)
+        .catch(err => console.log(err.message)) 
+}
+// fetch is not defined in node 
+// we will use axios
+// console.log(getUser().then(data => console.log(data.email)));
+
+module.exports = getUser
---
+++ functions/promise.test.js
@@ -0,0 +1,8 @@
+const getUser = require('./promise');
+
+test('email should be Shanna@melissa.tv', () => {
+    expect.assertions(1);
+    return getUser().then((data) => {
+        expect(data.email).toBe('Shanna@melissa.tv')
+    })
+});
